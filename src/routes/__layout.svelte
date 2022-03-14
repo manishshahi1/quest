2e3bbdf2b28897eee765a5ea7cfc51ee5ce46f53
Rohit Patil <rahulhimesh09@gmail.com>
2022-03-14 23:35:18 +0530

    create load fucntion in layout to pass posts to footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,32 @@
+<script context="module">
+	export async function load() {
+		const allPosts = import.meta.glob('/src/routes/calendar/event/*');
+		// console.table(allPosts);
+		let body = [];
+		for (let path in allPosts) {
+			body.push(
+				allPosts[path]().then(({ metadata }) => {
+					return { path, metadata };
+				})
+			);
+		}
+		let posts = await Promise.all(body);
+		// console.table(posts);
+		return {
+			props: {
+				posts
+			}
+		};
+	}
+</script>
+
 <script>
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import SearchModal from '$lib/components/SearchModal.svelte';
-	
+
 	//trim slashes from url
+	export let posts;
 </script>
 
 <Navbar />
@@ -11,4 +34,4 @@
 
 <slot><!-- optional fallback --></slot>
 
-<Footer />
+<Footer {posts} />
